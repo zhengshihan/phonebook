0a0ce6a38ad9d7ec47d0caa0c3c575e72ef62e49
zhengshihan <zhengshihan1998@outlook.com>
2024-07-31 19:21:07 +0100

    deploy

---
+++ README.md
@@ -0,0 +1,43 @@
+# My Express Project
+
+Welcome to the **My Express Project** repository! This project is a simple phonebook application built with Node.js and Express, allowing users to create, update, delete, and filter contact information.
+
+## Table of Contents
+
+- [My Express Project](#my-express-project)
+  - [Table of Contents](#table-of-contents)
+  - [Project Overview](#project-overview)
+  - [Features](#features)
+  - [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+
+## Project Overview
+
+This is a Node.js and Express application that serves as a basic backend for a phonebook application. Users can perform CRUD (Create, Read, Update, Delete) operations on contact entries. The app features RESTful API endpoints and a simple front-end built with React for interacting with the backend.
+
+## Features
+
+- **Add new contacts**: Create new entries with a name and phone number.
+- **Update existing contacts**: Update the phone number of an existing contact.
+- **Delete contacts**: Remove contacts from the phonebook.
+- **Filter contacts**: Search and filter contacts by name.
+
+## Getting Started
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+Ensure you have the following software installed on your system:
+
+- **Node.js**: [Download and install Node.js](https://nodejs.org/)
+- **npm**: Node.js comes with npm installed, but ensure it's up-to-date.
+
+### Installation
+
+1. **Clone the repository**
+
+   ```bash
+   git clone https://github.com/your-username/your-repo-name.git
+   cd your-repo-name
---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+app.use(express.json());
+
+app.use(morgan("tiny"));
+
+const cors = require("cors");
+
+app.use(cors());
+
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+app.get("/info", (request, response) => {
+  response.send(
+    `<h1>Phonebook has info for ${
+      persons.length
+    } people</h1><br/><h1>${new Date().toISOString()}</h1>`
+  );
+});
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "the name is missing",
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "the number is missing",
+    });
+  }
+
+  const nameExists = persons.some((person) => person.name === body.name);
+
+  if (nameExists) {
+    return response.status(400).json({ message: "Name must be unique." });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId().toString(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
